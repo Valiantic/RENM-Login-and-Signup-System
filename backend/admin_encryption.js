c200af15364ef97d07dda05064ecdc53d821032a
StevenMadali <stevenmadali17@gmail.com>
2024-10-20 22:58:40 +0800

    Add Admin Credential Encryption

---
+++ backend/admin_encryption.js
@@ -0,0 +1,10 @@
+
+// ADMIN CREDENTIALS ENCRYPTION
+// const bcrypt = require('bcryptjs');
+
+// // Hash the admin password
+// const password = 'Admin017!';
+// bcrypt.hash(password, 10, (err, hash) => {
+//   if (err) throw err;
+//   console.log(hash); // This will give you the hashed password
+// });
